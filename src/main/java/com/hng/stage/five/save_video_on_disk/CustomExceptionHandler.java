7a8c0e04907b0744067bf8f00a8c6b9981a1e6ed
Your name here <your_email@example.com>
2023-09-30 09:34:00 +0100

    added exception handler

---
+++ src/main/java/com/hng/stage/five/save_video_on_disk/CustomExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.todolist.utils.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<?> exceptionHandler(Exception e){
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+}
